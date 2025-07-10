dcf33811608fdb89be4f7b00fd7a6588f71d52d3
EnochArmah-Quaye <armahquayeenoch494@gmail.com>
2025-07-10 09:48:22 +0000

    3
    3

---
+++ src/main/java/com/ecom/productcatalog/controller/ProductController.java
@@ -0,0 +1,15 @@
+package com.ecom.productcatalog.controller;
+
+import com.ecom.productcatalog.service.ProductService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+}
