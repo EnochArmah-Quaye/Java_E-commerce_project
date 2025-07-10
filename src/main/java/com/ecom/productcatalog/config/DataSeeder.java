6c03dec0415fbb446c0b02b95b31b5b34d92631f
EnochArmah-Quaye <armahquayeenoch494@gmail.com>
2025-07-10 10:58:58 +0000

    4
    4

---
+++ src/main/java/com/ecom/productcatalog/config/DataSeeder.java
@@ -0,0 +1,42 @@
+package com.ecom.productcatalog.config;
+
+import com.ecom.productcatalog.model.Category;
+import com.ecom.productcatalog.repository.CategoryRepository;
+import com.ecom.productcatalog.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class DataSeeder implements CommandLineRunner {
+
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+
+    public DataSeeder(ProductRepository productRepository, CategoryRepository categoryRepository) {
+        this.productRepository = productRepository;
+        this.categoryRepository = categoryRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //Clear all exiting data
+        productRepository.deleteAll();
+        categoryRepository.deleteAll();
+
+        //Create Categories
+        Category electronics = new Category();
+        electronics.setName("Electronics");
+
+        Category clothing = new Category();
+        clothing.setName("Clothing");
+
+        Category home = new Category();
+        home.setName("Home and Kitchen");
+
+        categoryRepository.saveAll(Arrays.asList(electronics, clothing, home));
+
+        //Create Products
+    }
+}
--- src/main/java/com/ecom/productcatalog/controller/ProductController.java
+++ src/main/java/com/ecom/productcatalog/controller/ProductController.java
@@ -1,8 +1,12 @@
 package com.ecom.productcatalog.controller;
 
+import com.ecom.productcatalog.model.Product;
 import com.ecom.productcatalog.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -11,5 +15,15 @@ public class ProductController {
 
     public ProductController(ProductService productService) {
         this.productService = productService;
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/category/{categoryId}")
+    public List<Product> getAllProductsByCategory(Long categoryId){
+        return productService.getProductByCategory(categoryId);
     }
 }
